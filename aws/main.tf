cee6f9850f58be149435d7c4deaa05d51753ad20
saikishore8106 <145852787+saikishore8106@users.noreply.github.com>
2025-07-25 17:37:40 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,1 +1,59 @@
+# ------------------------
+# Provider Configuration
+# ------------------------
+provider "aws" {
+  region = var.aws_region
+}
+
+# ------------------------
+# EC2 Instances (Count = 4)
+# ------------------------
+resource "aws_instance" "example" {
+  count         = 4
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "${var.instance_name}-${count.index + 1}"
+  }
+}
+
+# ------------------------
+# Variables
+# ------------------------
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Base name for the EC2 instances"
+  type        = string
+  default     = "BasicEC2Instance"
+}
+
+# ------------------------
+# Outputs
+# ------------------------
+output "instance_ids" {
+  description = "IDs of the EC2 instances"
+  value       = [for instance in aws_instance.example : instance.id]
+}
+
+output "public_ips" {
+  description = "Public IPs of the EC2 instances"
+  value       = [for instance in aws_instance.example : instance.public_ip]
+}
 
